38531e331faa37353ea93fc815d883ccf7abd459
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2023-01-25 14:57:56 +0300

    Added AddTodoVCToViewController Protocol

---
+++ TodoAppWithProtocol/Protocols.swift
@@ -0,0 +1,12 @@
+//
+//  Protocols.swift
+//  TodoAppWithProtocol
+//
+//  Created by Yılmaz Yağız Dokumacı on 25.01.2023.
+//
+
+import Foundation
+
+protocol AddTodoVCToViewController {
+    func addTodo(todo:Todo)
+}
